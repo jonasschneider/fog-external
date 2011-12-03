62e7c41f6f0315d8839e64f5b88212c21cdd257f
Jonas Schneider <mail@jonasschneider.com>
2011-12-03 14:21:28 +0100

    refactor

--- spec/fog/bertrpc/storage_spec.rb
+++ spec/fog/bertrpc/storage_spec.rb
@@ -18,24 +18,28 @@ describe "fog-bertrpc" do
     BERTRPC::Service.stub(:new).and_return(service_mock)
   end
   
-  it "creates a directory" do
-    directory = storage.directories.create(
-      :key => 'mykey'
-    )
-  end
-  
-  it "lists directories" do
-    storage.directories.first.key.should == 'mykey'
-  end
-  
-  it "gets a directory" do
-    x = storage.directories.get('mykey')
-    x.should be_kind_of(Fog::Storage::Bertrpc::Directory)
-    x.key.should == 'mykey'
-  end
-  
-  it "returns nil for unknown directory keys" do
-    x = storage.directories.get('something_strange')
-    x.should be_nil
+  describe "#directories" do
+    it "#create creates a directory" do
+      directory = storage.directories.create(
+        :key => 'mykey'
+      )
+    end
+    
+    it "#all lists directories" do
+      storage.directories.all.first.key.should == 'mykey'
+    end
+    
+    describe "#get" do
+      it "gets a directory" do
+        x = storage.directories.get('mykey')
+        x.should be_kind_of(Fog::Storage::Bertrpc::Directory)
+        x.key.should == 'mykey'
+      end
+      
+      it "returns nil for unknown directory keys" do
+        x = storage.directories.get('something_strange')
+        x.should be_nil
+      end
+    end
   end
 end
